8d576d1de56456e143cb9fd610d90c3f72359e72
jhsiaomei <jw1314@gmail.com>
2016-01-04 21:09:56 -0600

    added users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215015730) do
+ActiveRecord::Schema.define(version: 20151227172649) do
 
   create_table "organizations", force: :cascade do |t|
     t.string   "school",     limit: 255
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151215015730) do
     t.string   "logo",       limit: 255
     t.string   "color",      limit: 255
     t.string   "motto",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "api_key",    limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
